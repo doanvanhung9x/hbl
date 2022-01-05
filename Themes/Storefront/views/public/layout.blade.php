58e7d77f6a03c491810719968b88cd63116d5636
Your Name <youremail@domain.com>
2022-01-05 16:21:39 +0700

    update

--- Themes/Storefront/views/public/layout.blade.php
+++ Themes/Storefront/views/public/layout.blade.php
@@ -28,8 +28,22 @@
           content="{{ asset('frontend/images/des.png') }}">
     <link rel="shortcut icon" href="{{ $favicon }}" />
     @stack('css')
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PQ8QWZ7');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQ8QWZ7"
+                      height="0" width="0" style="display:none;visibility:hidden">
+        </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('public.partials.header')
     @yield('content')
     @include('public.partials.footer')
