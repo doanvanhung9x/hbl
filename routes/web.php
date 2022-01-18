d1bce62d3858c6094e96b6c5a1b85430293c9ccb
Your Name <youremail@domain.com>
2022-01-18 14:27:48 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+//use Spatie\Sitemap\SitemapGenerator;
+//
+//SitemapGenerator::create('https://hblgroup.vn')->writeToFile(public_path('sitemap.xml'));
+
 Route::get('install/pre-installation', 'InstallController@preInstallation');
 Route::get('install/configuration', 'InstallController@getConfiguration');
 Route::post('install/configuration', 'InstallController@postConfiguration');
